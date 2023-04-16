a3553cc9f08ea8b954309e276f6ad25a0dd43727
soldwater <126317982+soldwater@users.noreply.github.com>
2023-04-16 15:43:39 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+[  
+ {    
+  "Users": 
+[ 
+  {        
+    "ID": "shaz",
+    "PASSWORD": "shaz123"
+  },
+  {  
+    "ID": "vava",
+    "PASSWORD": "vava333"
+  },
+  {
+  "ID": "adhil",
+  "PASSWORD": "adhi"
+  }    
+]
+   }
+]
